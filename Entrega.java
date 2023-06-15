cd56657fbc6a30814a3cbbfc56e64c3cfe0fbe39
Luz <119298515+luzsalva@users.noreply.github.com>
2023-06-15 21:34:12 +0200

    Nombres grupo

--- Entrega.java
+++ Entrega.java
@@ -32,9 +32,9 @@ import java.util.function.Predicate;
  *
  * Podeu fer aquesta entrega en grups de com a màxim 3 persones, i necessitareu com a minim Java 8.
  * Per entregar, posau a continuació els vostres noms i entregau únicament aquest fitxer.
- * - Nom 1:
- * - Nom 2:
- * - Nom 3:
+ * - Nom 1: Luz Salvá Castro 
+ * - Nom 2: Pablo Ramos Llabrérs 
+ * - Nom 3: Carlos Segura Hernando
  *
  * L'entrega es farà a través d'una tasca a l'Aula Digital que obrirem abans de la data que se us
  * hagui comunicat i vos recomanam que treballeu amb un fork d'aquest repositori per seguir més
